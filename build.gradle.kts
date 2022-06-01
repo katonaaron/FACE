fce5d39cd59751face287a51682e4559f6b1727a
Katona Áron <katonaaron01@gmail.com>
2022-06-01 14:26:41 +0300

    Add gradle dependencies and submodules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 import java.net.URI
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10" apply false
-    id("org.springframework.boot") version "2.6.4" apply false
+    kotlin("jvm") version "1.6.21" apply false
+    kotlin("multiplatform") version "1.6.21" apply false
+    kotlin("plugin.spring") version "1.6.21" apply false
+    kotlin("plugin.serialization") version "1.6.21" apply false
+    id("org.springframework.boot") version "2.6.7" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    id("com.github.node-gradle.node") version "3.2.1" apply false
+    id("org.openapi.generator") version "5.4.0" apply false
 }
 
 group = "com.katonaaron"
@@ -17,22 +21,22 @@ allprojects {
         mavenCentral()
         maven { url = URI("https://jitpack.io") }
     }
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-    apply(plugin = "java")
+//    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+//    apply(plugin = "java")
 
-    dependencies {
-        implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
-    }
+//    dependencies {
+// //        implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
+//    }
 
-    java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+//    java {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
+//    }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
+            jvmTarget = "11"
         }
     }
 }
