0b95eaf346e6cec8e9d26f62ba983822f15476d5
Katona Áron <katonaaron01@gmail.com>
2022-06-16 13:07:25 +0300

    Update ontology verbalizer proxy.

--- fact-checker-tools/src/main/kotlin/com/katonaaron/verbalizer/OwlVerbalizerProxy.kt
+++ fact-checker-tools/src/main/kotlin/com/katonaaron/verbalizer/OwlVerbalizerProxy.kt
@@ -6,17 +6,24 @@ import com.katonaaron.commons.axiomsToOntology
 import com.katonaaron.commons.toFormattedString
 import com.katonaaron.onto.OntologyVerbalizer
 import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat
+import org.semanticweb.owlapi.model.OWLAxiom
 import org.semanticweb.owlapi.model.OWLOntology
 
 class OwlVerbalizerProxy(url: String) : OntologyVerbalizer {
     private val verbalizer = VerbalizerWebservice(url)
 
     override fun verbalizeOntology(ontology: OWLOntology): String {
-        val newAxioms = ontology.axioms.map { it.axiomWithoutAnnotations }
+        val newAxioms = ontology.logicalAxioms.map {
+            it.axiomWithoutAnnotations
+        }
         val o = axiomsToOntology(newAxioms)
 
         return verbalizer.call(o.toFormattedString(OWLXMLDocumentFormat()), OutputType.ACE)
             .replace("\\n+".toRegex(), "\n")
             .replace("\\n+$".toRegex(), "")
+    }
+
+    override fun verbalizeAxiom(axiom: OWLAxiom): String {
+        return verbalizeOntology(axiomsToOntology(axiom))
     }
 }
